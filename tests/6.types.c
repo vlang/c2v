47c7ffe70360deee074b16bbd9af72c5a26daaca
Devin Bayer <dev@doubly.so>
2022-09-15 16:51:55 +0200

    fix capitalization for u16 and friends

--- tests/6.types.c
+++ tests/6.types.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// standard types should be lower-cased
+typedef unsigned short ushort;
+typedef float f32;
+typedef size_t size_t;
+typedef void *null;
+typedef size_t usize;
+typedef ptrdiff_t isize;
+typedef bool maybe;
+
+// not sure how this is supposed to work
+typedef intptr_t iptr;
 
 int main()
 {
     void *pointers[8];
+
     return 0;
 }
