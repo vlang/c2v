147c7714c8c16735bce6e44ac4b3d8eb8c40b961
Devin Bayer <dev@doubly.so>
2022-09-17 10:14:38 +0200

    normalize type names in test and attempt to fix macOS compatibility

--- tests/6.types.c
+++ tests/6.types.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-#include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
 
 // standard types should be lower-cased
-typedef unsigned short ushort;
-typedef float f32;
-typedef size_t size_t;
-typedef void *null;
-typedef size_t usize;
-typedef ptrdiff_t isize;
-typedef bool maybe;
+typedef unsigned short v_u16;
+typedef float v_f32;
+typedef size_t v_usize;
+typedef void *v_voidptr;
+typedef bool v_bool;
+
+// disabled for macOS/Linux compatibility
+//#include <stddef.h>
+//typedef ptrdiff_t v_isize;
 
 // not sure how this is supposed to work
-typedef intptr_t iptr;
+typedef intptr_t c_intptr_t;
 
 int main()
 {
