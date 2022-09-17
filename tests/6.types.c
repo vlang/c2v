6b8d69192711025c6f1dc9422abf2057fecd4f88
Devin Bayer <dev@doubly.so>
2022-09-17 10:56:37 -0400

    fix capitalization for u16 and friends (#69)
    * fix capitalization for u16 and friends
    
    * normalize type names in test and attempt to fix macOS compatibility

--- tests/6.types.c
+++ tests/6.types.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// standard types should be lower-cased
+typedef unsigned short v_u16;
+typedef float v_f32;
+typedef size_t v_usize;
+typedef void *v_voidptr;
+typedef bool v_bool;
+
+// disabled for macOS/Linux compatibility
+//#include <stddef.h>
+//typedef ptrdiff_t v_isize;
+
+// not sure how this is supposed to work
+typedef intptr_t c_intptr_t;
 
 int main()
 {
     void *pointers[8];
+
     return 0;
 }
