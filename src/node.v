203000bc393d4e132225560337c44aeb2ac66341
Artem Yurchenko <zumikq78@gmail.com>
2022-12-10 16:44:31 +0100

    Refactor: copyright comment was removed

--- src/node.v
+++ src/node.v
@@ -1,6 +1,3 @@
-// Copyright (c) 2022 Alexander Medvednikov. All rights reserved.
-// Use of this source code is governed by a GPL license that can
-// be found in the LICENSE file.
 module main
 
 // vfmt off
