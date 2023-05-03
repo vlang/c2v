811a8b7c320d1fda9a23d4a95ed5f84ccc1f2537
trollkarlen <robbelibobban@gmail.com>
2023-05-03 10:49:06 +0200

    Fix enum params in function calls and test runner warning
    Co-authored-by: Robert Marklund <robbelibobban@gmail.com>

--- tests/11.enum_default.c
+++ tests/11.enum_default.c
@@ -18,10 +18,18 @@ typedef enum myStrangeEnum {
 
 enum { J = 1 };
 
+void enum_func(enum myEnum a) {
+}
+
+void enum_func_const(const enum myEnum a) {
+}
+
 int main() {
     enum myEnum myEnumVar = A;
     myAnotherEnum myEnumVar2 = D;
     myStrangeEnum myEnumVar3 = G;
     int myIntVar = J;
+    enum_func(myEnumVar);
+    enum_func_const(myEnumVar);
     return 0;
 }
