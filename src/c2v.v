184d5bd440835096b1665669a63e52a6c6a7ee40
Dylan Donnell <dylan.donnell@student.griffith.ie>
2024-12-27 13:13:57 +0000

    feat: remove c89/99 keywords

--- src/c2v.v
+++ src/c2v.v
@@ -13,11 +13,10 @@ import datatypes
 const version = '0.4.1'
 
 // V keywords, that are not keywords in C:
-const v_keywords = ['as', 'asm', 'assert', 'atomic', 'break', 'const', 'continue', 'defer', 'else',
-	'enum', 'false', 'fn', 'for', 'go', 'goto', 'if', 'implements', 'import', 'in', 'interface',
-	'is', 'isreftype', 'lock', 'match', 'module', 'mut', 'none', 'or', 'pub', 'return', 'rlock',
-	'select', 'shared', 'sizeof', 'spawn', 'static', 'struct', 'true', 'type', 'typeof', 'union',
-	'unsafe', 'volatile', '__global', '__offsetof']
+const v_keywords = ['as', 'asm', 'assert', 'atomic', 'defer', 'false', 'fn', 'go', 'implements',
+	'import', 'in', 'interface', 'is', 'isreftype', 'lock', 'match', 'map', 'module', 'mut', 'none',
+	'or', 'pub', 'rlock', 'select', 'shared', 'spawn', 'string', 'true', 'type', 'typeof', 'unsafe',
+	'__global', '__offsetof']
 
 // libc fn definitions that have to be skipped (V already knows about them):
 const builtin_fn_names = ['fopen', 'puts', 'fflush', 'getline', 'printf', 'memset', 'atoi', 'memcpy',
